b218aadb903b0d582e86c7325f9fd31c6fdc3951
John Vresilovic <johnvresilovic@protonmail.com>
2022-11-30 12:03:31 -0500

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/greeting/:name', (req, res) => {
     res.send('Hey there ' + req.params.name + '!');
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    calculatedTip = req.params.total * (req.params.tipPercentage / 100)
+    res.send('If total is ' + req.params.total + ', then tip is ' + calculatedTip + '.');
+});
+
 app.listen(port,() => {
     console.log('Listening on port' , port);
 });
